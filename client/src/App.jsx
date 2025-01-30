c19938eb2185d8d464c91728a59d110dab69cd7e
Suryansh Singh <137262891+suryansh1440@users.noreply.github.com>
2025-01-30 18:08:49 +0530

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen bg-gray-900 text-red-500'>
+      <h1 className='text-3xl font-bold underline'>hello world</h1>
+    </div>
+  )
+}
+
+export default App
